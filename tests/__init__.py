04355ff5b5d773b54f2e5d253fa8cc8db89de732
Chris Wilson <christopher.david.wilson@gmail.com>
2024-07-24 13:29:40 -0700

    ci(pre-commit): add interrogate pre-commit hook
    Resolves #2

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,3 +1,5 @@
 # SPDX-FileCopyrightText: Chris Wilson <christopher.david.wilson@gmail.com>
 #
 # SPDX-License-Identifier: Apache-2.0 OR MIT
+
+"""Tests for the ``tinydantic`` package."""
--- tests/test_version.py
+++ tests/test_version.py
@@ -2,19 +2,29 @@
 #
 # SPDX-License-Identifier: Apache-2.0 OR MIT
 
-from importlib.metadata import version
+"""Version tests."""
+
+from importlib import metadata
 
 from packaging.version import parse
 
 from tinydantic import __version__
 
 
-class TestVersion:
+class TestPackageVersion:
+    """Package version tests."""
+
     def test_version_is_a_string(self):
+        """Package version is a string."""
         assert isinstance(__version__, str)
 
     def test_version_matches_metadata_version(self):
-        assert __version__ == version("tinydantic")
+        """Package version matches installed metadata."""
+        assert __version__ == metadata.version("tinydantic")
 
     def test_version_is_valid(self):
+        """Package version is compliant with PyPA Version specifiers.
+
+        See https://packaging.python.org/en/latest/specifications/version-specifiers/#version-specifiers
+        """
         parse(__version__)
